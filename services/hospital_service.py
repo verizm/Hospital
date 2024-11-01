575bfa6455d931b69115e935fc8ba59e170225cc
verizm <myemail@mail.ru>
2024-11-01 13:24:34 +0300

    fix bug

--- services/hospital_service.py
+++ services/hospital_service.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from validators.input_validator import InputValidator
 from patient_statuses import patient_statuses
 from patient_statuses import PatientStatuses
 from app_exceptions.validators_expection import ValidatorException
@@ -8,17 +7,20 @@ from app_exceptions.validators_expection import ValidatorException
 class HospitalService:
     def __init__(self, hospital_db: list):
         self.hospital_db = hospital_db
-        self.input_validator = InputValidator()
 
     def _request_patient_id(self) -> str:
         patient_id = input(f"Введите ID пациента: ").strip()
-        self.input_validator.check_patient_id(patient_id)
+        if not patient_id.isdigit() or int(patient_id) <= 0:
+            raise ValidatorException("Ошибка. ID пациента должно быть числом (целым, положительным)")
+
+        patient_index = int(patient_id) - 1
+        if len(self.hospital_db) <= int(patient_index) or self.hospital_db[patient_index] is None:
+            raise ValidatorException("Ошибка. В больнице нет пациента с таким ID")
+
         return patient_id
 
     def _get_status_id(self, patient_id: str) -> int:
         patient_index = int(patient_id) - 1
-        if len(self.hospital_db) <= int(patient_index) or self.hospital_db[patient_index] is None:
-            raise ValidatorException("Ошибка. В больнице нет пациента с таким ID")
         status_id = self.hospital_db[patient_index]
         return status_id
 
@@ -73,5 +75,6 @@ class HospitalService:
 
             if answer == "да":
                 self._update_status(patient_id, status_id=None)
+                print("Пациент выписан из больницы")
             else:
                 print(f"Пациент остался в статусе '{patient_statuses[status_id]}'")
